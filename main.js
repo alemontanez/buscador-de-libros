cac23e17d761b77d2fff3fd14023bcc27800f6c4
alemontanez <alemontanez99@gmail.com>
2024-08-23 16:38:29 -0300

    Se agregan validaciones para la busqueda y render de inicio

--- main.js
+++ main.js
@@ -8,8 +8,11 @@ let search = ''
 // Función que valida el valor de la búsqueda, en caso de no ser un string vacío ejecuta la búsqueda.
 const executeSearch = () => {
     search = input.value
-    const queryUrl = `https://api.itbook.store/1.0/search/${search}`
-    if (search === '') {
+    // Se formatea los espacios del string de búsqueda para no tener errores al insertarlo en la url mas adelante.
+    const formattedSearch = search.replaceAll(' ', '')
+    const queryUrl = `https://api.itbook.store/1.0/search/${formattedSearch}`
+    // Validamos que la búsqueda no sea vacía para evitar ese render innecesario.
+    if (formattedSearch === '') {
         return
     } else {
         fetchBooks(queryUrl)
@@ -32,32 +35,45 @@ const fetchBooks = async(url) => {
     try {
         const response = await fetch(url)
         const data = await response.json()
-        books = data.books
-        renderBooks(books)
+        // Almacenamos en books el array books obtenido de data.
+        const books = data.books
+        renderBooks(books, url)
     } catch (error) {
         console.error(error)
     }
 }
 
 // Función donde validamos los datos obtenidos de la api según la búsqueda del usuario. En caso de ser correcto renderizamos en el DOM las card creadas para mostrar la información.
-const renderBooks = (books) => {
+const renderBooks = (books, url) => {
     booksList.innerHTML = ''
     const fragment = document.createDocumentFragment()
-        if (books.length === 0) {
-            title.textContent = 'No se encontraron coincidencias'
-            booksList.appendChild(p)
-        } else {
+    // Validamos que el array books no esté vacío. E ncaso de estar vacío renderizamos el mensaje en el cual indicamos que no se encontraron libros que coincidan con la búsqueda.
+    if (books.length === 0) {
+        title.textContent = 'No se encontraron coincidencias'
+        booksList.appendChild(p)
+    } else {
+        // Corroboramos que la url no sea la del render inicial, para indicar si cambiamos o no el título.
+        if (url !== 'https://api.itbook.store/1.0/new') {
             title.textContent = `Libros relacionados con "${search}"`
-            books.forEach(book => {
-                const bookCard = document.createElement('li')
-                bookCard.classList.add('book-card')
-
-                const cardImage = Object.assign(document.createElement('img'), { src: book.image })
-                const cardTitle = Object.assign(document.createElement('h3'), { textContent: book.title })
-                const cardSubtitle = Object.assign(document.createElement('p'), { textContent: book.subtitle })
-                bookCard.append(cardImage, cardTitle, cardSubtitle)
-                fragment.appendChild(bookCard)
-            })
-            booksList.appendChild(fragment)
         }
+        // Recorremos el array books y creamos la card correspondiente para cada item, para luego pasarlo en nuestro fragment. Una vez finalizado el bucle renderizamos la lista con todas las card en el dom.
+        books.forEach(book => {
+            const bookCard = document.createElement('li')
+            bookCard.classList.add('book-card')
+
+            const cardImage = Object.assign(document.createElement('img'), { src: book.image })
+            const cardTitle = Object.assign(document.createElement('h3'), { textContent: book.title })
+            const cardSubtitle = Object.assign(document.createElement('p'), { textContent: book.subtitle })
+            bookCard.append(cardImage, cardTitle, cardSubtitle)
+            fragment.appendChild(bookCard)
+        })
+        booksList.appendChild(fragment)
+    }
 }
+
+// Render al cargar la página por primera vez
+const initialRender = () => {
+    const initialUrl = 'https://api.itbook.store/1.0/new'
+    fetchBooks(initialUrl)
+}
+initialRender()
