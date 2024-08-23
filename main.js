2779b0818c37cae2befb6476e1c925a0889c1293
alemontanez <alemontanez99@gmail.com>
2024-08-22 22:26:31 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,63 @@
+const container = document.getElementById('container')
+const booksList = document.getElementById('books-list')
+const searchButton = document.getElementById('search')
+const input = document.getElementById('input')
+const title = document.getElementById('title')
+let search = ''
+
+// Función que valida el valor de la búsqueda, en caso de no ser un string vacío ejecuta la búsqueda.
+const executeSearch = () => {
+    search = input.value
+    const queryUrl = `https://api.itbook.store/1.0/search/${search}`
+    if (search === '') {
+        return
+    } else {
+        fetchBooks(queryUrl)
+        input.value = ''
+    }
+}
+
+// Listeners Click y Enter para la barra de búsqueda.
+searchButton.addEventListener('click', () => {
+    executeSearch()
+})
+input.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        executeSearch()
+    }
+})
+
+// Función asíncrona donde realizamos el fetch de la api con nuestra url dinámica.
+const fetchBooks = async(url) => {
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+        books = data.books
+        renderBooks(books)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+// Función donde validamos los datos obtenidos de la api según la búsqueda del usuario. En caso de ser correcto renderizamos en el DOM las card creadas para mostrar la información.
+const renderBooks = (books) => {
+    booksList.innerHTML = ''
+    const fragment = document.createDocumentFragment()
+        if (books.length === 0) {
+            title.textContent = 'No se encontraron coincidencias'
+            booksList.appendChild(p)
+        } else {
+            title.textContent = `Libros relacionados con "${search}"`
+            books.forEach(book => {
+                const bookCard = document.createElement('li')
+                bookCard.classList.add('book-card')
+
+                const cardImage = Object.assign(document.createElement('img'), { src: book.image })
+                const cardTitle = Object.assign(document.createElement('h3'), { textContent: book.title })
+                const cardSubtitle = Object.assign(document.createElement('p'), { textContent: book.subtitle })
+                bookCard.append(cardImage, cardTitle, cardSubtitle)
+                fragment.appendChild(bookCard)
+            })
+            booksList.appendChild(fragment)
+        }
+}
