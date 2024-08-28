9ad3fba5898a8cf5c8d5efdc1c70444f47b33f73
alemontanez <alemontanez99@gmail.com>
2024-08-28 03:17:00 -0300

    Nuevo diseño responsive + paginación funcional (prev, next, firstPage, lastPage buttons), faltan detalles.

--- main.js
+++ main.js
@@ -3,77 +3,134 @@ const booksList = document.getElementById('books-list')
 const searchButton = document.getElementById('search')
 const input = document.getElementById('input')
 const title = document.getElementById('title')
-let search = ''
+const paginationTemplate = document.getElementById('template')
 
-// Función que valida el valor de la búsqueda, en caso de no ser un string vacío ejecuta la búsqueda.
-const executeSearch = () => {
-    search = input.value
-    // Se formatea los espacios del string de búsqueda para no tener errores al insertarlo en la url mas adelante.
-    const formattedSearch = search.replaceAll(' ', '')
-    const queryUrl = `https://api.itbook.store/1.0/search/${formattedSearch}`
-    // Validamos que la búsqueda no sea vacía para evitar ese render innecesario.
-    if (formattedSearch === '') {
-        return
-    } else {
-        fetchBooks(queryUrl)
-        input.value = ''
+// { 1 }
+// Función que procesa la búsqueda solicitada desde el navbar.
+const executeSearch = async(search, page = 1) => {
+    const formattedSearch = search.trim().replaceAll(' ', '-')
+    if (formattedSearch === '') return
+    
+    const url = getFormattedSearch(formattedSearch, page)
+    const data = await getBooksData(url)
+    if (data) {
+        renderBooks(data, url, data.total, formattedSearch, page)
     }
+    // Despejamos la barra de búsqueda.
+    input.value = ''
+    window.scrollTo(0, 0)
+    console.log('Current url:', url)
 }
 
-// Listeners Click y Enter para la barra de búsqueda.
+const getFormattedSearch = (search, page) => {
+    return `https://api.itbook.store/1.0/search/${search}/${page}`
+}
+
+// Listeners Click y Enter para labarra  de búsqueda.
 searchButton.addEventListener('click', () => {
-    executeSearch()
+    title.textContent = 'Cargando...'
+    executeSearch(input.value, 1)
 })
 input.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
-        executeSearch()
+        title.textContent = 'Cargando...'
+        executeSearch(input.value, 1)
     }
 })
 
-// Función asíncrona donde realizamos el fetch de la api con nuestra url dinámica.
-const fetchBooks = async(url) => {
+// { 2 }
+// Realizamos el fetch de la api con nuestra url dinámica.
+const getBooksData = async(url) => {
     try {
         const response = await fetch(url)
         const data = await response.json()
-        // Almacenamos en books el array books obtenido de data.
-        const books = data.books
-        renderBooks(books, url)
+        return data
     } catch (error) {
         console.error(error)
     }
 }
 
-// Función donde validamos los datos obtenidos de la api según la búsqueda del usuario. En caso de ser correcto renderizamos en el DOM las card creadas para mostrar la información.
-const renderBooks = (books, url) => {
+// { 3 }
+// Validamos los datos obtenidos de la api según la búsqueda del usuario. En caso de ser correcto renderizamos en el DOM las card creadas para mostrar la información.
+const renderBooks = (data, url, totalBooks, search, currentPage) => {
     booksList.innerHTML = ''
-    const fragment = document.createDocumentFragment()
-    // Validamos que el array books no esté vacío. E ncaso de estar vacío renderizamos el mensaje en el cual indicamos que no se encontraron libros que coincidan con la búsqueda.
-    if (books.length === 0) {
-        title.textContent = 'No se encontraron coincidencias'
-        booksList.appendChild(p)
+    if (url === 'https://api.itbook.store/1.0/new') {
+        renderBookCards(data.books)
     } else {
-        // Corroboramos que la url no sea la del render inicial, para indicar si cambiamos o no el título.
-        if (url !== 'https://api.itbook.store/1.0/new') {
-            title.textContent = `Libros relacionados con "${search}"`
-        }
-        // Recorremos el array books y creamos la card correspondiente para cada item, para luego pasarlo en nuestro fragment. Una vez finalizado el bucle renderizamos la lista con todas las card en el dom.
-        books.forEach(book => {
-            const bookCard = document.createElement('li')
-            bookCard.classList.add('book-card')
+        // Se valida que el array books no esté vacío.
+        if (data.books.length === 0) {
+            title.textContent = 'No se encontraron coincidencias.'
+        } else {
+            search = search.replaceAll('-', ' ')
+            title.textContent = `Se encontraron ${totalBooks} libros relacionados con "${search}":`
+            let totalPages = Math.ceil(totalBooks/10)
+            renderBookCards(data.books)
 
-            const cardImage = Object.assign(document.createElement('img'), { src: book.image })
-            const cardTitle = Object.assign(document.createElement('h3'), { textContent: book.title })
-            const cardSubtitle = Object.assign(document.createElement('p'), { textContent: book.subtitle })
-            bookCard.append(cardImage, cardTitle, cardSubtitle)
-            fragment.appendChild(bookCard)
-        })
-        booksList.appendChild(fragment)
+            renderPagination()
+            const lastPageButton = document.getElementById('last-page-button')
+            lastPageButton.textContent = totalPages
+
+            // Lógica para el funcionamiento de los botones, con delegación de eventos.
+            const paginationList = document.getElementById('pagination-list')
+            paginationList.addEventListener('click', (e) => {
+                if (e.target.matches('#prev-page-button')) {
+                    if (currentPage > 1) {
+                        executeSearch(search, currentPage - 1)
+                    }
+                }
+                if (e.target.matches('#next-page-button')) {
+                    if (currentPage < totalPages) {
+                        executeSearch(search, currentPage + 1)
+                    }
+                }
+                if (e.target.matches('#first-page-button')) {
+                    if (currentPage !== 1) {
+                        executeSearch(search, 1)
+                    }
+                }
+                if (e.target.matches('#last-page-button')) {
+                    if (currentPage !== totalPages) {
+                        executeSearch(search, totalPages)
+                    }
+                }
+            })
+        }
     }
 }
 
-// Render al cargar la página por primera vez
-const initialRender = () => {
+const renderPagination = () => {
+    if (document.getElementById('pagination-list')) {
+        document.getElementById('pagination-list').remove()
+    }
+    const paginationClone = paginationTemplate.content.cloneNode(true)
+    container.appendChild(paginationClone)
+}
+
+// { 4 }
+// Función que renderiza las cards con info de los libros recibidos.
+const renderBookCards = (books) => {
+    const fragment = document.createDocumentFragment()
+    books.forEach(book => {
+        const bookCard = document.createElement('li')
+        bookCard.classList.add('book-card')
+        const cardImage = Object.assign(document.createElement('img'), { src: book.image })
+        const cardTitle = Object.assign(document.createElement('h3'), { textContent: book.title })
+        const cardSubtitle = Object.assign(document.createElement('p'), { textContent: book.subtitle })
+        bookCard.append(cardImage, cardTitle, cardSubtitle)
+        fragment.appendChild(bookCard)
+    })
+    booksList.appendChild(fragment)
+}
+
+// { 0 }
+// Render al cargar la página por primera vez.
+const initialRender = async() => {
     const initialUrl = 'https://api.itbook.store/1.0/new'
-    fetchBooks(initialUrl)
+
+    const data = await getBooksData(initialUrl)
+    if (data) {
+        // Se pasa el cuarto argumento como undefined ya que no se utiliza en este caso.
+        renderBooks(data, initialUrl, data.total, undefined)
+    }
 }
 initialRender()
